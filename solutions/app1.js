8c792dbf3fa1aa97b283b26b5ecae8cbb0ae1687
Hamburger Réka <Rka>
2022-08-17 18:08:58 +0200

    js2-003

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,12 +1,36 @@
 const user = {
   firstName: 'John',
   lastName: 'Doe',
+ job:'unknown',
 };
 
+const firstName = user.firstName;
+const lastName = user.lastName;
+ const job = user.job;
+
+ if (isNaN(job)) {
+  user.job= 'unknown';
+ }
+
+
+ if (isNaN(firstName)) {
+  user.firstName= 'unknown';
+ }
+
+ if (isNaN(lastName)) {
+  user.lastName= 'unknown';
+ }
+
+console.log(firstName,lastName,job);
 //
 
-// export {
-//   firstName,
-//   lastName,
-//   job,
-// };
+export {
+ firstName,
+ lastName,
+ job,
+};
+
+
+
+
+
--- solutions/app3.js
+++ solutions/app3.js
@@ -1,2 +1,13 @@
+
 const names = ['John', 'Jane', 'Judith', 'Jennifer', 'Jeremiah', 'Johnny'];
+
+const [,a , ,b , ,c] = names;
+
+
+export {
+    names,
+    a,
+    b,
+    c,
+   };
 
